a46dd961ecf4b8a2abdef5c0cfe1acbc5728c70b
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-01-16 20:41:52 -0800

    played with counting functions, if, if else

--- main.js
+++ main.js
@@ -1,2 +1,45 @@
-var sum = 3 + 4;
-console.log ('hello console', sum);
+var x = 15;
+
+if(x > 12) {
+    
+    console.log ('hello console', x);
+    console.log('groovin with if');
+}
+
+else {
+    console.log('too small');
+}
+
+// 1. initialize
+// 2. condition
+// 3. post loop action
+for(var i = 0; i < x; i++) {
+
+    if(i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+
+function sayHello() {
+    console.log('hi');
+}
+
+if(x % 2 === 0) {
+    sayHello();
+}
+
+// eslint-disable-next-line
+function evenOrOdd() {
+    var input = document.getElementById('number');
+    console.log(input.value);
+
+    var y = input.value;
+
+    if(y % 2 === 0) {
+        console.log('even');
+    }
+    else {
+        console.log('odd');
+    }
+}
